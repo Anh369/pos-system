62251442dfbb57674c3303691b497b637fc27250
NTADEV <nguyentheanhdeveloper369999@gmail.com>
2025-09-17 11:44:21 +0700

    create spring boot project pos system

---
+++ src/main/java/com/theanhdev/TheanhdevPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.theanhdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheanhdevPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheanhdevPosSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theanhdev/TheanhdevPosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theanhdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheanhdevPosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
